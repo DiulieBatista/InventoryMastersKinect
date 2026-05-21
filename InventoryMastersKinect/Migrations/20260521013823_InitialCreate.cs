e910705f2e29f3256fb2541925d34fa4adc1cb8f
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-05-20 22:58:52 -0300

    Instalação dos pacotes referentes ao projeto, Upload das configurações e pasta Migrations, criação do banco.

---
+++ InventoryMastersKinect/Migrations/20260521013823_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryMastersKinect.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MedicoesVolume",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    VolumeCm3 = table.Column<double>(type: "REAL", nullable: false),
+                    DataHora = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    KinectLigado = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Calibrado = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicoesVolume", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MedicoesVolume");
+        }
+    }
+}
