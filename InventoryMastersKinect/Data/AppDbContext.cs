fe42acea7d56568b0d3305af3ee95b87bd6755cc
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-05-20 21:09:54 -0300

    Implementação da estrutua do banco de dados no sqlite, criando o AppDbContext.

---
+++ InventoryMastersKinect/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using InventoryMastersKinect.Model;
+
+namespace InventoryMastersKinect.Data
+{  
+    public class AppDbContext : DbContext
+    {
+        public DbSet<MedicaoVolume> MedicoesVolume { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=medicoes.db");
+        }
+    }
+}
